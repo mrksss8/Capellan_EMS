6b51c9d9fe1334c8ad568cc85a1262ee1ac3f152
mrksss8 <devmrksss@gmail.com>
2022-05-27 01:57:07 +0800

    added- active school year and sem

--- app/Models/Active_SchoolYearAndSem.php
+++ app/Models/Active_SchoolYearAndSem.php
@@ -10,4 +10,13 @@ class Active_SchoolYearAndSem extends Model
     use HasFactory;
 
     protected $guarded = [''];
+
+
+    public function schoolyear(){
+        return $this->belongsTo(SchoolYear::class, 'active_SY_id');
+    }
+
+    public function sem(){
+        return $this->belongsTo(Sem::class, 'active_sem_id');
+    }
 }
