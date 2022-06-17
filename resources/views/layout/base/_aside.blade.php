707f81a6a723fa36e18f974a5ec47ae0a8bfebae
Kheyzel <kheyzelnugoy@gmail.com>
2022-06-17 15:29:24 +0800

    added - alumni

--- resources/views/layout/base/_aside.blade.php
+++ resources/views/layout/base/_aside.blade.php
@@ -135,7 +135,7 @@
                                 
                             </li>
                             <li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
-                                <a href="#" class="menu-link menu-toggle">
+                                <a href="{{ route('alumni.index') }}"  class="menu-link menu-toggle">
                                     <i class="menu-bullet menu-bullet-line">
                                         <span></span>
                                     </i>
