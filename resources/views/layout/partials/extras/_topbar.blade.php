df5e6d4a94253678ecc7dbce1e65d18acd1278de
Kheyzel <kheyzelnugoy@gmail.com>
2022-06-21 14:56:05 +0800

    fixed - topbar

--- resources/views/layout/partials/extras/_topbar.blade.php
+++ resources/views/layout/partials/extras/_topbar.blade.php
@@ -1,32 +1,5 @@
 {{-- Topbar --}}
 <div class="topbar">
-
-    {{-- Search --}}
-    @if (config('layout.extras.search.display'))
-        @if (config('layout.extras.search.layout') == 'offcanvas')
-            <div class="topbar-item">
-                <div class="btn btn-icon btn-clean btn-lg mr-1" id="kt_quick_search_toggle">
-
-                    {{ Metronic::getSVG('media/svg/icons/General/Search.svg', 'svg-icon-xl svg-icon-primary') }}
-                </div>
-            </div>
-        @else
-            <div class="dropdown" id="kt_quick_search_toggle">
-                {{-- Toggle --}}
-                <div class="topbar-item" data-toggle="dropdown" data-offset="10px,0px">
-                    <div class="btn btn-icon btn-clean btn-lg btn-dropdown mr-1">
-                        {{ Metronic::getSVG('media/svg/icons/General/Search.svg', 'svg-icon-xl svg-icon-primary') }}
-                    </div>
-                </div>
-
-                {{-- Dropdown --}}
-                <div class="dropdown-menu p-0 m-0 dropdown-menu-right dropdown-menu-anim-up dropdown-menu-lg">
-                    @include('layout.partials.extras.dropdown._search-dropdown')
-                </div>
-            </div>
-        @endif
-    @endif
-
     {{-- mission --}}
     @if (config('layout.extras.notifications.display'))
         @if (config('layout.extras.notifications.layout') == 'offcanvas')
@@ -197,12 +170,11 @@
     @if (config('layout.extras.user.display'))
         @if (config('layout.extras.user.layout') == 'offcanvas')
             <div class="topbar-item">
-                <div class="btn btn-icon w-auto btn-clean d-flex align-items-center btn-lg px-2"
-                    id="kt_quick_user_toggle">
+                <div class="btn btn-icon w-auto btn-clean d-flex align-items-center btn-lg px-2">
                     <span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,</span>
-                    <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">Sean</span>
+                    <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">{{Auth::user()->name}}</span>
                     <span class="symbol symbol-35 symbol-light-success">
-                        <span class="symbol-label font-size-h5 font-weight-bold">S</span>
+                       
                     </span>
                 </div>
             </div>
@@ -212,7 +184,7 @@
                 <div class="topbar-item" data-toggle="dropdown" data-offset="0px,0px">
                     <div class="btn btn-icon w-auto btn-clean d-flex align-items-center btn-lg px-2">
                         <span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,</span>
-                        <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">Sean</span>
+                        <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">{{Auth::user()->name}}</span>
                         <span class="symbol symbol-35 symbol-light-success">
                             <span class="symbol-label font-size-h5 font-weight-bold">S</span>
                         </span>
